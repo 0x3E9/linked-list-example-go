0d23c54787dbeb8763541ce1aeee0eead86bcdf6
Louis <145569683+0x3E9@users.noreply.github.com>
2023-12-20 00:07:27 +0000

    Create main.go

---
+++ cmd/main.go
@@ -0,0 +1,53 @@
+package main
+
+type Node struct {
+	Val  int
+	Next *Node
+}
+
+type LinkedList struct {
+	Head *Node
+}
+
+func (l *LinkedList) Add(val int) {
+	if l.Head == nil {
+		l.Head = &Node{Val: val}
+		return
+	}
+
+	l.Head = &Node{
+		Val:  val,
+		Next: l.Head,
+	}
+}
+
+func (l *LinkedList) Pop() int {
+	if l.Head == nil {
+		return 0
+	}
+
+	previous := l.Head
+	l.Head = l.Head.Next
+
+	return previous.Val
+}
+
+func (l *LinkedList) Remove() {
+	if l.Head == nil {
+		return
+	}
+
+	l.Head = l.Head.Next
+}
+
+func main() {
+	linkedList := LinkedList{}
+
+	for i := 0; i < 10; i++ {
+		linkedList.Add(i + 1)
+	}
+
+	for linkedList.Head != nil {
+		println(linkedList.Pop())
+	}
+}
